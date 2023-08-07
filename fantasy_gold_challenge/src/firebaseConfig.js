89340993704cee401c09c4960ebe515e5584833b
nahnahAJ <nahnahahjuwah@gmail.com>
2023-08-07 16:24:57 +0000

    Create config file for firebase

---
+++ fantasy_gold_challenge/src/firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ_j1WCz5c2qpJJlnEM6xbBAss_OmlVyU",
+  authDomain: "fantasy-gold-challenge.firebaseapp.com",
+  projectId: "fantasy-gold-challenge",
+  storageBucket: "fantasy-gold-challenge.appspot.com",
+  messagingSenderId: "190898939402",
+  appId: "1:190898939402:web:1983a9575bb6fd440f61f3",
+  measurementId: "G-4YP63553DQ"
+};
+
+export default firebaseConfig;
